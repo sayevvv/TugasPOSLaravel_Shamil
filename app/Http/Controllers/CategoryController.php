b48115988fb93c70a2481d86ad7f8a4d1cc848f0
sayevvv <shamilbasayev10@gmail.com>
2025-02-25 07:12:06 +0700

    Update Controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Category::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|unique:categories|max:255',
+        ]);
+        $category = Category::create($request->all());
+        return response()->json($category, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        return response()->json($category);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Category $category)
+    {
+        $request->validate([
+            'name' => 'required|max:255|unique:categories,name,' . $category->id,
+        ]);
+        $category->update($request->all());
+        return response()->json($category);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->json(null, 204);
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
-    <h1>Point of Sales </h1>
+    <h1 class="font-bold text-3xl">Point of Sales </h1>
+        <div class="flex justify-center items-center py-3 mx-4 bg-blue-100 rounded-xl">
+            <p>Product A</p>
+        </div>
 </x-layout>
